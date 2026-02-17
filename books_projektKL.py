7a5e05bc717b3ac6f97c6845a55d2c55c6f16431
Lorinc22 <56290775+Lorinc22@users.noreply.github.com>
2026-02-17 19:57:59 +0100

    Menu commit 3 , database commit 4

--- books_projektKL.py
+++ books_projektKL.py
@@ -1,3 +1,11 @@
+
+config ={
+    'host': 'postgres',
+    'database': 'postgres',
+    'user': 'postgres',
+    'password': 'postgres',
+}
+
 def menu():
     control = input("""
 [1] Regisztracio
@@ -18,9 +26,18 @@ def menu():
 def register():
     name = input("Add meg a teljes neved:")
     username = input("Add meg a felhasznalo neved:")
+    email = input("Add meg az emailed:")
     password = input("Add meg a jelszavad:")
     birth_date = input("Szuletesi datum (YYYY-MM-DD): ")
     phonenumber = input("Telefonszam: ")
+
+    if len(password) < 8:
+        print("A jelszo tul rovid")
+    if not phonenumber.isdigit():
+        print("A Telefonszam csak szamjegyekbol allhat!")
+    if "@" not in email or "." not in email:
+        print("Hibas email formatum")
+
 def login():
     username = input("Add meg a felhasznalo neved:")
     password = input("Add meg a jelszavad:")
